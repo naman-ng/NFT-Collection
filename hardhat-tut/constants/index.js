f9713788a8a4f5459d4ca7642d35576769001b30
naman-ng <naman21171@iiitd.ac.in>
2022-10-25 19:48:59 +0530

    changed metadata

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = '0x56787904086EC3311FeEefd02B9D15E28212CE78';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-colecton.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
